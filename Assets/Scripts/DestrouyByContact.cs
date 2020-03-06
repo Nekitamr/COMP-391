4b2dd5a052739773df4f6ab99444273a3e1c8c64
Nikita Melyanov <nmelyano@my.centennialcollege.ca>
2020-03-06 13:51:50 -0500

    Applied explosions to asteroids

---
+++ Assets/Scripts/DestrouyByContact.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestrouyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //create an explosion and position it properly
+        Instantiate(explosion, transform.position, transform.rotation);
+        //destroy "this" object
+        //destroy the "other" object
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+}
